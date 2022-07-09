cea7e6e8ff6ab2a70af4962b466adeb70fe71740
Rosian Pop <poprosian@gmail.com>
2022-07-09 16:27:12 +0300

    Added softeng and improved script code

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ class Colors:
     RED = "\033[91m"
     GREEN = "\033[92m"
     END = "\033[0m"
+    YELLOW = "\33[33m"
 
 
 class Test:
@@ -53,6 +54,13 @@ class Test:
         return all_questions_dict
         
 
+
+def print_correct_answers(questions, question, answers):
+    print("Correct answers: ")
+    for answer in answers:
+        if questions[question][answer]:
+            print(answer)
+
 def main():
     test_file = input('Input the file name of the test: ')
     if test_file != "all" and not path.exists(test_file):
@@ -60,33 +68,45 @@ def main():
         sys.exit(1)
 
     ti = Test(test_file)
-    keys = list(ti.questions.keys())
-    shuffle(keys)
-    for key in keys:
-        print(key)
-        answer_keys = list(ti.questions[key].keys())
-        shuffle(answer_keys)
-        for (answer_key, letter) in zip(answer_keys, ti.letter_to_index.keys()):
-            print(f"    {letter}.{answer_key}")
+    # Gets the questions from the ti TestInstance variable and shuffles them, then loops through them
+    questions = list(ti.questions.keys())
+    shuffle(questions)
+    for question in questions:
+        print(question)
+        # Gets the answers as a list and then shuffles them
+        answers = list(ti.questions[question].keys())
+        total_correct_answers = 0
+        user_correct_answers = 0
+        user_wrong_answers = 0
+        
+        shuffle(answers)
+        # Zips through the answers and the letter_to_index keys to be able to assign a letter to the answers.
+        for (answer, letter) in zip(answers, ti.letter_to_index.keys()):
+            print(f"    {letter}.{answer}")
+            if ti.questions[question][answer]:
+                total_correct_answers += 1
         input_values = input("Answers: ")
 
-        total_wrong_answers = 0
+        # Loops through the values inputted by the user, maps it to the answer's index and checks if it is wrong.
         for val in input_values:
             idx = ti.letter_to_index[val]
-            if not ti.questions[key][answer_keys[idx]]:
-                print(f"{Colors.RED}Wrong answer: {answer_keys[idx]}{Colors.END}")
-                total_wrong_answers += 1
-
-        if total_wrong_answers == 0:
+            if not ti.questions[question][answers[idx]]:
+                print(f"{Colors.RED}Wrong answer: {answers[idx]}{Colors.END}")
+                user_wrong_answers += 1
+            else:
+                user_correct_answers += 1
+                
+                
+                
+        if user_wrong_answers == 0 and user_correct_answers == total_correct_answers:
             print(f"{Colors.GREEN}Correct!{Colors.END}")
+        elif user_correct_answers != correct_answers and user_wrong_answers == 0:
+            print(f"{Colors.YELLOW}All your answers are correct, but there are more correct answers you did not choose. {Colors.END}")
+            print_correct_answers(ti.questions, question, answers)
         else:
-            print("Correct answers: ", end=' ')
-            for answer_key in answer_keys:
-                if ti.questions[key][answer_key]:
-                    print(answer_key, end='; ')
-        print(" ")
-        print(" ")
-        print(" ")
+            print_correct_answers(ti.questions, question, answers)
+        
+        print("\n\n\n")
 
 
 if __name__ == '__main__':
